e560b3f0bf1a12e97f280cc145731c4a6cf25b68
Alkha77 <kuliah.alvi@gmail.com>
2024-03-29 14:10:22 +0700

    First Commit

---
+++ application/views/biodata.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href=<? ="http://localhost/biodata/alvi/assets/css/bootstrap.css">
+    <title>Welcome</title>
+</head>
+<center>
+<body>
+<nav>
+<div class="start">
+    <a href="index.html">Biodata Diri</a>
+</div>
+</nav>
+
+<br><br>
+
+<header class="header">
+<figure class="text-center">
+    <img
+        src="images.jpg"
+        alt="profil saya"
+    >
+    <figcaption>Alvi Khairullah Andhika</figcaption>
+    <strong>(17210153)</strong>
+</figure>
+</header>
+
+<br><br><br>
+
+<main>
+<hr>
+<article class="content">
+    <p> Nama Lengkap: Alvi Khairullah Andhika</p>
+    <p> Kelas: 17.6A.26 </p>
+    <p>Alamat: Jl. Kemanggisan Utama</p>
+    <p>Hobby: Membaca </p>
+    <p>Cita - Cita: Programmer</p>
+    <p>Pekerjaan: Freelance</p>
+</article>
+</main>
+
+<br><br>
+</body>
+</center>
+</html>
